5a399ad62fb841066c3db14ff2b19202bd9b5063
Krishna Venkatramani (Rain Work GPGPassphrase) <krishna@rain.bh>
2022-08-22 22:25:05 +0400

    Added AccountHeaderView

--- ArtOcean/Views/Accounts/AccountHeaderView.swift
+++ ArtOcean/Views/Accounts/AccountHeaderView.swift
@@ -8,32 +8,56 @@
 import Foundation
 import UIKit
 
-class AccountHeaderView:UIView{
+class AccountHeaderView: UIView {
     
     private var handler:(() -> Void)? = nil
     private var imgHeightConstraint:NSLayoutConstraint? = nil
     private var imgTopAnchor:NSLayoutConstraint? = nil
     private var height:CGFloat = .zero
+	private var headerHeight: CGFloat = .zero
     
-    private let headerImageView:CustomImageView = {
-        let image = CustomImageView(named: "CustomProfileImage", cornerRadius: 0, maskedCorners: nil)
+    private lazy var headerImageView: CustomImageView = {
+        let image = CustomImageView(cornerRadius: 0, maskedCorners: nil)
         return image
     }()
     
-    private lazy var backButton:CustomButton = {
-        let button  = CustomButton(systemName: "chevron.left", handler: self.handler, autolayout: true)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
+	private lazy var backButton: CustomImageButton = {
+		let button: CustomImageButton = .backButton { [weak self] in
+			self?.handler?()
+		}
+		return button
+	}()
+	
+	private lazy var stretchyHeader: StreachyHeaderView = { .init(height: headerHeight, innerView: headerImageView) }()
+	
+	private lazy var userProfileImage: UIImageView = {
+		let imageView = UIImageView()
+		imageView.cornerRadius = 20
+		imageView.clipsToBounds = true
+		imageView.image = .loadingBackgroundImage
+		return imageView
+	}()
+	
+	private lazy var userHeader: UIStackView = {
+		let stack: UIStackView = .HStack(views: [userProfileImage, artTitleLabel], spacing: 12, aligmment: .center)
+		userProfileImage.setAspectRatio(1)
+		return stack
+	}()
+	
+	private lazy var artTitleLabel: UILabel = { .init() }()
+	
+	private var gradientView: UIView = { .init() }()
     
-    
-    init(height:CGFloat = 200,handler:@escaping (() -> Void)) {
-        self.height = height
-        super.init(frame: .zero)
+	private var viewSize: CGSize { .init(width: .totalWidth, height: height) }
+	
+	init(height:CGFloat = 200, headerHeight: CGFloat = 180,handler:@escaping (() -> Void)) {
+		super.init(frame: .zero)
+		self.height = height
+		self.headerHeight = headerHeight
         self.handler = handler
-        self.translatesAutoresizingMaskIntoConstraints = false
-        self.setupViews()
-        self.setupLayout()
+		setupViews()
+		setupLayout()
+		buildUI()
     }
     
     required init?(coder: NSCoder) {
@@ -41,31 +65,35 @@ class AccountHeaderView:UIView{
     }
     
     
-    func setupViews(){
-        self.addSubview(self.headerImageView)
-        self.addSubview(self.backButton)
-    }
+	func setupViews(){
+		addSubview(stretchyHeader)
+		addSubview(backButton)
+		addSubview(userHeader)
+	}
+	
+	func buildUI(){
+		UIImage.loadImage(url: .testBackdropImage, for: headerImageView, at: \.image)
+		UIImage.loadImage(url: .testProfileImage, for: userProfileImage, at: \.image)
+		"CryptoDon".heading1(color: .appBlackColor).renderInto(target: artTitleLabel)
+	}
     
     func setupLayout(){
-        self.headerImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-        self.headerImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-        self.imgTopAnchor = self.headerImageView.topAnchor.constraint(equalTo: self.topAnchor)
-        self.imgTopAnchor?.isActive = true
-        self.imgHeightConstraint = self.headerImageView.heightAnchor.constraint(equalToConstant: self.height)
-        self.imgHeightConstraint?.isActive = true
+		setFrameLayout(childView: stretchyHeader, alignment: .top)
         
-        self.backButton.leadingAnchor.constraint(equalToSystemSpacingAfter: self.leadingAnchor, multiplier: 2).isActive = true
-        self.backButton.topAnchor.constraint(equalToSystemSpacingBelow: self.topAnchor, multiplier: 5).isActive = true
-        self.backButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
-        self.backButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+		setFrameLayout(childView: backButton, alignment: .topLeading, paddingFactor: .init(top: 40, left: 16, bottom: 0, right: 0))
+		setFrameConstraints(size: .init(width: .totalWidth, height: height))
+		
+		setWidthForChildWithPadding(userHeader, paddingFactor: 2)
+		setFrameLayout(childView: userHeader, alignment: .bottom, paddingFactor: .init(vertical: 24))
+		userHeader.setHeightWithPriority(60,priority: .required)
     }
     
-    public func viewAnimationWithScroll(_ scrollView:UIScrollView){
-        if scrollView.contentOffset.y < 0 {
-            self.headerImageView.clipsToBounds = false
-            self.imgTopAnchor?.constant = scrollView.contentOffset.y
-            self.imgHeightConstraint?.constant = max(self.height - scrollView.contentOffset.y,height)
-            
-        }
-    }
+	public func viewAnimationWithScroll(_ scrollView:UIScrollView){
+		guard scrollView.contentOffset.y < 0  else { return }
+		stretchyHeader.stretchOnScroll(scrollView)
+		let alpha: CGFloat = 1 - (0...100).precent(abs(scrollView.contentOffset.y))
+		gradientView.alpha = alpha
+		backButton.alpha = alpha
+		userHeader.alpha = alpha
+	}
 }
